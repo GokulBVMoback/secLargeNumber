69bdbe67fed8e6a5981589d49837afdffcdd1f24
GokulBVMoback <gokulbv@moback.com>
2022-09-17 15:33:24 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace secLargeNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = new int[10];
+            Console.WriteLine("Enter the numbers:");
+            for (int i = 0; i <10; i++)
+            {
+                num[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Array.Sort(num);
+            Array.Reverse(num);
+            Console.WriteLine("The Second Largest number of the Array is:" + num[1]);
+            Console.WriteLine();
+        }
+    }
+}
